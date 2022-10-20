2b6f0a622f8f0d79c168eff0b005217a325a1cb8
Naomi Mercado <mercadotx@Naomis-MacBook-Air-2.local>
2022-10-20 18:07:13 -0500

    spotipy functional test and started driver

--- src/Controller.py
+++ src/Controller.py
@@ -1,43 +1,60 @@
-# Runs the tool
+"""
+Author: Isaiah Mercado
+Purpose: Driver file
+Desired Outcome: Connects to user's spotify account and updates playlist based on user prefs
+"""
+import spotipy
+import datetime
+from datetime import timedelta
+from Preferences import Preferences
+from Account import Account
+
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = "user-library-read"
 
 # initialize a spotify client
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-# fetch a user
-
-# fetch the playlists
+# fetch a FAKE user
+prefs = Preferences("7", "ALBUM", "SAVE")
+userPlaylists = sp.current_user_playlists(limit=50, offset=0)
+user = Account(prefs, userPlaylists, 0)
 
 # iterate through the playlists
+for playlist in userPlaylists:
 
-# check the user prefs
+    # compare lastUpdated for each playlist to the refreshPeriod and the current datetime
+    now = datetime.datetime.now()
+    minimumUpdateDate = user.lastUpdate + timedelta(days=prefs.refreshPeriod)
+    if(now < minimumUpdateDate):
+        continue
+    # iterate through the songs in the playlist -- fix syntax
+    for song in playlist:
+        # call the songSelection method - pass the Song obj and the Update Style
 
-# compare lastUpdated for each playlist to the refreshPeriod and the current datetime
+        # song selection method should
 
-# iterate through the songs in the playlist
+            # fetch the full album or the artist's discography
 
-# call the songSelection method - pass the Song obj and the Update Style
+            # decide on a song to update with
 
-# song selection method should
+            # add the song to a new playlist
 
-    # fetch the full album or the artist's discography
+    # new playlist should be created by now
 
-    # decide on a song to update with
+    # final decision phase begins
 
-    # add the song to a new playlist
+    # show the user the new playlist
 
-# new playlist should be created by now
+    # ask user if they want to...
+        # update old playlist
+        # delete the old playlist and create the new one
+        # allow auto-update to decide based on their chosen preferences
 
-# final decision phase begins
-
-# show the user the new playlist
-
-# ask user if they want to...
-    # update old playlist
-    # delete the old playlist and create the new one
-    # allow auto-update to decide based on their chosen preferences
-
-# carry out the final decision
-    # update function (old playlist, new playlist)
-    # delete/create function (old playlist, new playlist)
+    # carry out the final decision
+        # update function (old playlist, new playlist)
+        # delete/create function (old playlist, new playlist)
 
 # move on to another playlist
 
--- src/Models/Account.py
+++ src/Models/Account.py
@@ -1,6 +1,5 @@
 class Account:
-  def __init__(self, username, preferences, playlists, lastUpdate):
-    self.username = username
+  def __init__(self, preferences, playlists, lastUpdate):
     self.preferences = preferences
     self.playlists = playlists
     self.lastUpdate = lastUpdate
