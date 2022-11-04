76c6c4db18a8b90724c552ecd122a1668915ff6a
Isaiah Mercado <isaiahmercado@Isaiahs-iMac.local>
2022-11-04 12:09:26 -0500

    user can now select which song to update from a list

--- src/Controller.py
+++ src/Controller.py
@@ -12,6 +12,60 @@ from spotipy.oauth2 import SpotifyOAuth
 
 SPOTIPY_CLIENT_ID='fe921ddf76d440e48f8766f8d040e71d'
 SPOTIPY_CLIENT_SECRET='464ab682558e4d3ea49be40d4efc2294'
+
+
+"""
+isDuplicate
+Goal: Check for duplicates of songs within a playlist
+song: title of track (String)
+playlist: list of track titles (List of Strings)
+
+returns true if song is a duplicate, false if not found in playlist
+"""
+def isDuplicate(song, playlist):
+    if(song in playlist):
+        return True
+    return False
+
+"""
+pickPlaylistToUpdate
+Goal: Make user decide which singular playlist they want to update
+
+Reason: Temporary functionality for testing
+Notes: Will update this function to communicate with the front-end
+
+:userPlaylists - list of all the current user's playlists
+Returns : Playlist object
+"""
+def pickPlaylistToUpdate(userPlaylists):
+    print("Please choose a playlist by entering the number next to it.")
+    playlistIndex = 0
+    for playlist in userPlaylists:
+        print(str(playlistIndex) + " : " + playlist["name"])
+        playlistIndex+=1
+
+    indexOfPlaylist = input('Your Selection: ')
+    return userPlaylists[int(indexOfPlaylist)]
+
+"""
+timeToUpdate
+
+goal: check if enough time has passed since last update to merit an update
+parameters:
+    - user (object)
+    
+:returns
+    - true if we should update
+    - false if we should not update
+"""
+
+def timeToUpdate():
+    now = datetime.datetime.now()
+    minimumUpdateDate = user.lastUpdate + timedelta(days=prefs.refreshPeriod)
+    if (now < minimumUpdateDate):
+        return True
+    return False
+
 """
 pickNewSong
 
@@ -24,37 +78,6 @@ parameters
 Notes:
 We don't allow the ALBUM point of commonality for singles
 """
-
-"""
-Goal: Check for duplicates of songs within a playlist
-song: title of track (String)
-playlist: list of track titles (List of Strings)
-
-returns true if song is a duplicate, false if not found in playlist
-"""
-def isDuplicate(song, playlist):
-    if(song in playlist):
-        return True
-    return False
-"""
-Goal: Make user decide which singular playlist they want to update
-
-Reason: Temporary functionality for testing
-Notes: Will update this function to communicate with the front-end
-
-:userPlaylists - list of all the current user's playlists
-Returns : Playlist object
-"""
-def pickPlaylistToUpdate(userPlaylists):
-    print("Please choose a playlist by entering the number next to it.")
-
-    playlistIndex = 0
-    for playlist in userPlaylists:
-        print(playlistIndex + " : " + playlist["name"])
-
-    playlistToUpdate = input('Your Selection: ')
-
-
 def pickNewSong(seed, playlistTracks, updatedTracks, pointOfCommonality):
     albumID = seed['track']["album"]["id"]
     albumTracks = sp.album_tracks(albumID)
@@ -82,7 +105,6 @@ def pickNewSong(seed, playlistTracks, updatedTracks, pointOfCommonality):
         if(not isDuplicate(track["name"], trackNamesInPlaylist) and not isDuplicate(track["name"], trackNamesInNewPlaylist)):
             print(track["name"])
             return track
-
     # Default track to return - may be a duplicate
     return sortedByPopularity[0]
 
@@ -99,33 +121,29 @@ userPlaylists = sp.current_user_playlists(limit=50, offset=0)["items"]
 user = Account(prefs, userPlaylists, datetime.datetime(2020, 4, 1), userID)
 
 # iterate through the playlists
-for playlist in userPlaylists:
-    # compare lastUpdated for each playlist to the refreshPeriod and the current datetime
-    now = datetime.datetime.now()
-    minimumUpdateDate = user.lastUpdate + timedelta(days=prefs.refreshPeriod)
-    if(now < minimumUpdateDate):
-        continue
+playlist = pickPlaylistToUpdate(userPlaylists)
+# compare lastUpdated for each playlist to the refreshPeriod and the current datetime
 
-    # name of new/temp playlist -- in the future we should use a version or date naming convention (ex: name_v1, name_sept10)
-    existingName = playlist["name"]
-    updatedName = existingName + "_UPDATED"
-    updatedDescription = "newest version of " + existingName
-    print(updatedName)
-    updatedPlaylist = sp.user_playlist_create(user.userID, updatedName, public=False, collaborative=True, description=updatedDescription)
 
-    # iterate through the songs in the playlist -- fix syntax
-    playlistTracks = sp.playlist_items(playlist["id"])["items"]
-    for song in playlistTracks:
-        updatedPlaylistTracks = sp.playlist_items(updatedPlaylist["id"])["items"]
-        # call the songSelection method - pass the Song obj and the Update Style
-        # song selection method should
-        newSong = pickNewSong(song, playlistTracks, updatedPlaylistTracks, user.preferences.updateStyle)
-        print(newSong["name"])
-        sp.playlist_add_items(updatedPlaylist["id"], items=[newSong["uri"]], position=None)
-    if (user.preferences.saveOrUpdate == "UPDATE"):
-        sp.current_user_unfollow_playlist(playlist["id"])
-        sp.playlist_change_details(playlist["id"], name=playlist["name"])
-    break
+# name of new/temp playlist -- in the future we should use a version or date naming convention (ex: name_v1, name_sept10)
+existingName = playlist["name"]
+updatedName = existingName + "_UPDATED"
+updatedDescription = "newest version of " + existingName
+print(updatedName)
+updatedPlaylist = sp.user_playlist_create(user.userID, updatedName, public=False, collaborative=True, description=updatedDescription)
+
+# iterate through the songs in the playlist -- fix syntax
+playlistTracks = sp.playlist_items(playlist["id"])["items"]
+for song in playlistTracks:
+    updatedPlaylistTracks = sp.playlist_items(updatedPlaylist["id"])["items"]
+    # call the songSelection method - pass the Song obj and the Update Style
+    # song selection method should
+    newSong = pickNewSong(song, playlistTracks, updatedPlaylistTracks, user.preferences.updateStyle)
+    print(newSong["name"])
+    sp.playlist_add_items(updatedPlaylist["id"], items=[newSong["uri"]], position=None)
+if (user.preferences.saveOrUpdate == "UPDATE"):
+    sp.current_user_unfollow_playlist(playlist["id"])
+    sp.playlist_change_details(playlist["id"], name=playlist["name"])
             # fetch the full album or the artist's discography
 
             # decide on a song to update with
